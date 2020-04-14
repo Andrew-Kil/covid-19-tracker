684685a3dd3d4f80c55472057769ffaf3fbac79c
Andrew KIl <kil.andrew@gmail.com>
2020-04-13 22:22:27 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -9,18 +9,28 @@ import styles from "./App.module.css";
 class App extends React.Component {
   state = {
     data: {},
+    country: "",
   };
+
   async componentDidMount() {
     const fetchedData = await fetchData();
     this.setState({ data: fetchedData });
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
     return (
       <div className={styles.container}>
         <Cards data={data}></Cards>
-        <CountryPicker></CountryPicker>
-        <Chart></Chart>
+        <CountryPicker
+          handleCountryChange={this.handleCountryChange}></CountryPicker>
+        <Chart data={data} country={country}></Chart>
       </div>
     );
   }
